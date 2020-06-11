28cda08d69af9eef378b26c7ee4ed02e48689fe8
obara_hikaru <obahika1@gmail.com>
2020-06-11 21:23:52 +0900

    no message

---
+++ tunedox/src/test/java/com/tune/dox/TunedoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tune.dox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunedoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
